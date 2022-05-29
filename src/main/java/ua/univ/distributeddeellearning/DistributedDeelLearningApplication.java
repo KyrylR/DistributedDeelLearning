dfd9f58cf1472b526cc27c1ad68d3c621fe88dd0
Kyrylo Riabov <kyryl.ryabov@gmail.com>
2022-05-29 23:39:49 +0300

    Initial commit

---
+++ src/main/java/ua/univ/distributeddeellearning/DistributedDeelLearningApplication.java
@@ -0,0 +1,13 @@
+package ua.univ.distributeddeellearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedDeelLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedDeelLearningApplication.class, args);
+    }
+
+}
